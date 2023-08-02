0ef093d7cd78bbfce55e590fd972065857985675
Andre Carreiro <andcarreiro@gmail.com>
2023-08-02 11:56:14 -0300

    Projeto Criado

---
+++ src/test/java/com/andrecarreiro/curso/CursomcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrecarreiro.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
